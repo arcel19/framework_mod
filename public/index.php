47570829564815f00ec21a003d4a6d02832920e4
arcel martial <kontcheu23@gmail.com>
2024-02-12 13:25:20 +0100

    Include function.php and dd($app) at the end of the file. (generated by blackbox.ai )

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
-echo "<pre>";
-print_r($_SERVER);
- echo "</pre>";
+include __DIR__ . '/../src/App/function.php';
 
  $app = include __DIR__ . '/../src/App/boostrap.php';
 
  $app->run();
+
+ dd($app);
