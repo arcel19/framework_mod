b0c6a284f963bd6cb0f58a715bd78da571f684d6
arcel martial <kontcheu23@gmail.com>
2024-02-12 14:08:43 +0100

    Normalize path in router (generated by blackbox.ai )

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -6,9 +6,17 @@ namespace Framework;
 class Router {
     private array $routes = [];
     public function add(string $method ,string $path){
+        $path = $this->normalizePath($path);
         $this->routes = [
             'path' => $path,
             'method' => strtoupper($method)
         ]; 
+    }
+
+    public function normalizePath(string $path):string
+    {
+        $path = trim($path, '/');
+        $path = "/{$path}/";
+        return $path;
     }
 }
