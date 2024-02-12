08755bd503face95e5425c9836646c3f5a0f9f56
arcel martial <kontcheu23@gmail.com>
2024-02-12 15:07:55 +0100

    Changed the HomeController route to use the class syntax. Added a Router class that normalizes the path and dispatches the request to the appropriate controller and method. (generated by blackbox.ai )

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -11,7 +11,11 @@ class App {
     }
     public function run()
     {
-        echo"Application is Running";
+        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $this->router->dispatch($path, $method);
+
     }
 
     public function get(string $path, array $controller)
